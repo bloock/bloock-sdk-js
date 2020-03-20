d925a0a65f1b5958e54b16493604b05364e36642
Marc Baqué <mbaque@enchainte.com>
2020-03-20 15:17:18 +0100

    Added Web3 Events integration

--- src/comms/web3.service.ts
+++ src/comms/web3.service.ts
@@ -1,18 +1,57 @@
 import Web3 from "web3";
-import { WEB3_PROVIDER, CONTRACT_ADDRESS, CONTRACT_ABI } from "../utils/constants";
+import { WEB3_PROVIDER, CONTRACT_ADDRESS, CONTRACT_ABI, ETHERSCAN_URL } from "../utils/constants";
 import Utils from "../utils/utils";
+import { EventEmitter } from "events";
 
 export default class Web3Service {
+    private static web3 = new Web3(new Web3.providers.WebsocketProvider(WEB3_PROVIDER));
+    private static contract = new Web3Service.web3.eth.Contract(JSON.parse(CONTRACT_ABI), CONTRACT_ADDRESS);
 
-    public static validateRoot(root: Uint8Array): Promise<boolean> {
-        // const abi = CONTRACT_ABI;
-        // const web3 = new Web3(new Web3.providers.HttpProvider(WEB3_PROVIDER));
-        // const contract = new web3.eth.Contract(JSON.parse(abi), CONTRACT_ADDRESS);
+    public static validateRoot(root: Uint8Array): Promise<boolean> {        
+        const rootString = Web3Service.web3.utils.asciiToHex(Utils.bytesToString(root));
         
-        // const rootString = web3.utils.asciiToHex(Utils.bytesToString(root));
-        
-        // return contract.methods.getRoot(rootString).call();
+        return Web3Service.contract.methods.getRoot(rootString).call();
+    }
 
-        return Promise.resolve(true);
+    public static getPastEvents(): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            Web3Service.contract.getPastEvents('InsertedRoot', {fromBlock: 0, toBlock: 'latest'})
+                .then((events: any[]) => {
+                    events = events.map(event => {
+                        return {
+                            event: event.event,
+                            transactionHash: event.transactionHash,
+                            root: event.returnValues._root,
+                            transactionUrl: `${ETHERSCAN_URL}${event.transactionHash}`
+                        }
+                    });
+                    resolve(events);
+                });
+        })
+    }
+
+    public static getEvents(): EventEmitter {
+        let emiter = new EventEmitter();
+
+        let listener =  Web3Service.contract.events.InsertedRoot();
+
+        listener.on('data', function(event: any){
+            event = {
+                event: event.event,
+                transactionHash: event.transactionHash,
+                root: event.returnValues._root,
+                transactionUrl: `${ETHERSCAN_URL}${event.transactionHash}`
+            }
+
+            emiter.emit('data', event);
+        })
+        listener.on('changed', function(event: any){
+            emiter.emit('changed', event)
+        })
+        listener.on('error', (error: any) => {
+            emiter.emit('error', error)
+        });
+
+        return emiter;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import EnchainteClient from "./enchainte-client";
 import Hash from "./utils/hash";
+import Web3Service from "./comms/web3.service";
 
-export { EnchainteClient, Hash };
+export { EnchainteClient, Hash, Web3Service };
